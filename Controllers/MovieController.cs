293e478779aa104a5fcc32e6b1a4da23ed6c88f6
Horváth Márton <marcihorat@gmail.com>
2024-10-14 22:07:15 +0200

    #3
    GetMovies és GetMovie controllerek kialakítása

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using Filmek.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmek.Controllers
+{
+    public class MovieController : Controller
+    {
+        /// <summary>
+        /// osztályváltozót hozunk létre az interface-nek
+        /// </summary>
+        private readonly IMovieService _ims;
+
+        /// <summary>
+        /// Konstruktor
+        /// paraméterként átvesszük az interface-t
+        /// </summary>
+        /// <param name="ims">A megvalósítandó interface</param>
+        public MovieController(IMovieService ims) 
+        {
+              _ims = ims;
+        }
+        /// <summary>
+        /// Index oldal
+        /// </summary>
+        /// <returns>A filmek listájával tér vissza</returns>
+        public IActionResult Index()
+        {
+            return View(_ims.getMovies()); 
+        }
+
+        public IActionResult GetMovie(int id) 
+        {
+            return View(_ims.getMovie(id));
+        }
+    }
+}
